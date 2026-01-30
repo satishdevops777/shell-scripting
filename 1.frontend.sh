458898de998942b9c6ef1deaafb05086299c1626
SATISH REDDY SATTI <satishdevops3789@gmail.com>
2026-01-30 21:10:37 +0530

    Update 1.frontend.sh

--- 1.frontend.sh
+++ 1.frontend.sh
@@ -9,7 +9,7 @@ echo -e "${GREEN}Enable and Start Nginx ${RESET}"
 systemctl enable nginx 
 systemctl start nginx 
 
-echo -e"${GREEN}Remove and download app code ${RESET}"
+echo -e "${GREEN}Remove and download app code ${RESET}"
 rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
